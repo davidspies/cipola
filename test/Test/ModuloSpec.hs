40b9d4f95e6896ef01998d4e0c17b6c5300294ef
David Spies <dnspies@gmail.com>
2018-01-02 17:56:04 -0800

    Generalize

--- test/Test/ModuloSpec.hs
+++ test/Test/ModuloSpec.hs
@@ -1,16 +1,33 @@
+{-# LANGUAGE PatternSynonyms #-}
+{-# LANGUAGE TypeOperators   #-}
+
 module Test.ModuloSpec(spec) where
 
+import           Data.List       (foldl', inits, tails)
 import           Modulo          (crt)
 import           Test.Hspec
-import           Test.QuickCheck (arbitrary, choose, getSize, property,
-                                  suchThat, (===))
+import           Test.QuickCheck
 
 spec :: Spec
 spec = describe "crt" $
-  it "should return Nothing when moduli can't be unified" $ property $ do
-    s <- toInteger <$> getSize
-    g <- choose (2, 2 + s)
-    m <- choose (1, 1 + s)
-    n <- choose (1, 1 + s)
-    (a, b) <- arbitrary `suchThat` (\(a, b) -> a `mod` g /= b `mod` g)
-    return $ crt [(a, m * g), (b, n * g)] === Nothing
+  it "should get the right result" $
+    property $ \(CRTList ans) -> case crt ans of
+        Nothing       -> classify False "unified" $ property (not (unifies ans))
+        Just (res, m) -> classify True "unified" $
+          conjoin [res `mod` n === a `mod` n | (a, n) <- ans] .&&.
+          m === foldl' lcm 1 (map snd ans)
+
+newtype CRTList = CRTList [(Integer, Integer)]
+  deriving (Show)
+
+instance Arbitrary CRTList where
+  arbitrary = sized $ \s -> fmap CRTList $ listOf $ do
+    n <- choose (1, 1 + toInteger s)
+    a <- arbitrary
+    return (a, n)
+  shrink (CRTList ans) =
+    map CRTList $ zipWith (++) (init $ inits ans) (tail $ tails ans)
+
+unifies :: [(Integer, Integer)] -> Bool
+unifies ans =
+  and [let g = gcd m n in a `mod` g == b `mod` g | (a, m) <- ans, (b, n) <- ans]
